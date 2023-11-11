fb4f20adefcf7986411600e6b3b1174405305e1c
nibukdk <nibeshkhadka@gmail.com>
2023-11-11 12:19:32 +0200

    Empty Config constants

---
+++ backend/config_4_tutorial.js
@@ -0,0 +1,7 @@
+var GPT_SECRET_KEY = "";
+var FORM_FOLDER_ID = "";
+var DOCS_FOLDER_ID = "";
+var TEMPLATE_ID = "";
+var APPOINTMENT_CALENDAR_ID = "";
+var MARK_REVIEWED_IMAGE_LINK_ID = "";
+var SCRIPT_URL = "";
